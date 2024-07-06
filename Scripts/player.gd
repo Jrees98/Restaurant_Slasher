f1c46cc3ad02193059d0c1f86406b62f0c2dbe5d
Jacob Rees <reescodes@gmail.com>
2024-07-06 17:31:44 -0600

    add player movement script

---
+++ Scripts/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+const ACCELERATION = 2800
+const FRICTION = 500
+const MAX_SPEED = 250
+
+func _physics_process(delta):
+	move(delta)
+	
+
+func move(delta):
+	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if input_vector == Vector2.ZERO:
+		apply_friction(FRICTION * delta)
+	else:
+		apply_movement(input_vector * ACCELERATION * delta)
+	move_and_slide()
+	
+func apply_friction(amount) -> void:
+	if velocity.length() > amount:
+		velocity -= velocity.normalized() * amount
+	else:
+		velocity = Vector2.ZERO
+		
+func apply_movement(amount) -> void:
+	velocity += amount
+	velocity = velocity.limit_length(MAX_SPEED)
